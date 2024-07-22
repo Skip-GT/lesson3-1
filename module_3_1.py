6e91f63952ca46f559ea0fdea2c0de4a6a534fd0
Skip-GT <yaroslav.vasilev.2015@gmail.com>
2024-07-22 15:25:46 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,26 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    st = len(string), string.upper(), string.lower()
+    return st
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    st = string.lower()
+    for i in list_to_search:
+        if st in i.lower():
+            return True
+    return False
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
